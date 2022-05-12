88bd33dbcbf4b7b129fc05d8c35892bf8ab9bacf
jurski <juris.svarnovics@gmail.com>
2022-05-12 10:52:12 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <section className='section about-section'>
+        <h1 className='section-title'>About us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio
+          quidem repellat harum est vitae officiis corrupti nulla iusto, quam
+          eos nam illum quisquam cumque possimus nisi blanditiis, fugiat
+          similique laborum tenetur repellendus reiciendis. Velit laboriosam sed
+          sit fuga voluptate!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
